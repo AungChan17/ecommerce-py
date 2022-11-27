27b3e0c4dd68ccb17ce76c377855e9779f3d8100
AungChan17 <chanmyae1792003@gmail.com>
2022-11-27 12:34:07 +0630

    first commit

---
+++ main/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from .models import Banner,Category,Brand,Product,ProductAttribute
+from django.db.models import Max,Min
+
+# Create your views here.
+
+# home page
+def home(request):
+    banners=Banner.objects.all().order_by('-id')
+    data=Product.objects.filter(is_featured=True).order_by('-id')
+    return render(request,'index.html',{'data':data,'banners':banners})
+
+#Category 
+def category_list(request):
+    data = Category.objects.all().order_by('-id')
+    return render(request,'category_list.html',{'data':data})
+
+#Brand
+def brand_list(request):
+    data=Brand.objects.all().order_by('-id')
+    return render(request,'brand_list.html',{'data':data})
+
+#Product List
+def product_list(request):
+    data=Product.objects.all().order_by('-id')
+    min_price=ProductAttribute.objects.aggregate(Min('price'))
+    max_price=ProductAttribute.objects.aggregate(Max('price'))
+    return render(request,'product_list.html',
+        {
+            'data':data,
+            'min_price':min_price,
+            'max_price':max_price
+        }
+        )
+
+
+#Product List According to Category
+def category_product_list(request,cat_id):
+    category=Category.objects.get(id=cat_id)
+    data=Product.objects.filter(category=category).order_by('-id')
+    return render(request,'category_product_list.html',
+        {
+            'data':data,
+        }
+        )
+
+
+#Product List According to Brand
+def brand_product_list(request,brand_id):
+    brand=Brand.objects.get(id=brand_id)
+    data=Product.objects.filter(brand=brand).order_by('-id')
+    return render(request,'category_product_list.html',
+        {
+            'data':data,
+        }
+        )
+
+
+#Product Detail
+def product_detail(request,slug,id):
+    product=Product.objects.get(id=id)
+    related_products=Product.objects.filter(category=product.category).exclude(id=id)[:4] # show related products
+    return render(request,'product_detail.html',{'data':product,'related':related_products})
+
+
+
+# Search page
+def search(request):
+    q=request.GET['q']
+    data=Product.objects.filter(title__icontains=q).order_by('-id')
+    return render(request,'search.html',{'data':data})
