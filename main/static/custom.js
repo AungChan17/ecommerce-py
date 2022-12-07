6344d99c3d23f011a2ee1ef2dc70105bcd5cb7c8
AungChan17 <chanmyae1792003@gmail.com>
2022-12-07 22:34:29 +0630

    add to cart

--- main/static/custom.js
+++ main/static/custom.js
@@ -28,5 +28,125 @@ $(document).ready(function(){
 			}
 		});
 		// End
-    })
-})
+    });
+
+	// Product Variation
+	$(".choose-size").hide();
+
+	// Show size according to selected color
+	$(".choose-color").on('click',function(){
+		$(".choose-size").removeClass('active');
+		$(".choose-color").removeClass('focused');
+		$(this).addClass('focused');
+
+		var _color=$(this).attr('data-color');
+
+		$(".choose-size").hide();
+		$(".color"+_color).show();
+		$(".color"+_color).first().addClass('active');
+
+		var _price=$(".color"+_color).first().attr('data-price');
+		$(".product-price").text(_price);
+
+	});
+	// End
+
+	// Show the price according to selected size
+	$(".choose-size").on('click',function(){
+		$(".choose-size").removeClass('active');
+		$(this).addClass('active');
+
+		var _price=$(this).attr('data-price');
+		$(".product-price").text(_price);
+	})
+	// End
+
+	// Show the first selected color
+	$(".choose-color").first().addClass('focused');
+	var _color=$(".choose-color").first().attr('data-color');
+	var _price=$(".choose-size").first().attr('data-price');
+
+	$(".color"+_color).show();
+	$(".color"+_color).first().addClass('active');
+	$(".product-price").text(_price);
+
+	// Add to cart
+	$(document).on('click',"#addToCartBtn",function(){
+		var _vm=$(this);
+		var _qty=$("#productQty").val();
+		var _productId=$(".product-id").val();
+		var _productImage=$(".product-image").val();
+		var _productTitle=$(".product-title").val();
+		var _productPrice=$(".product-price").text();
+		// Ajax
+		$.ajax({
+            url:'/add-to-cart',
+            data:{
+				'id':_productId,
+				'image':_productImage,
+				'qty':_qty,
+				'title':_productTitle,
+				'price':_productPrice
+			},
+            dataType:'json',
+            beforeSend:function(){
+				_vm.attr('disabled',true);
+            },
+            success:function(res){
+                $("cart-list").text(res.totalitems);
+				_vm.attr('disabled',false);
+            }
+        });
+		// End
+	});
+	// End
+
+	// Delete item from cart
+	$(document).on('click','.delete-item',function(){
+		var _pId=$(this).attr('data-item');
+		var _vm=$(this);
+		// Ajax
+		$.ajax({
+            url:'/delete-from-cart',
+            data:{
+				'id':_pId,
+			},
+            dataType:'json',
+            beforeSend:function(){
+				_vm.attr('disabled',true);
+            },
+            success:function(res){
+                $("cart-list").text(res.totalitems);
+				_vm.attr('disabled',false);
+				$("#cartList").html(res.data);
+            }
+        });
+		// End
+	});
+
+	// Update item from cart
+	$(document).on('click','.update-item',function(){
+		var _pId=$(this).attr('data-item');
+		var _pQty=$(".product-qty-"+_pId).val();
+		var _vm=$(this);
+		// Ajax
+		$.ajax({
+			url:'/update-cart',
+			data:{
+				'id':_pId,
+				'qty':_pQty,
+			},
+			dataType:'json',
+			beforeSend:function(){
+				_vm.attr('disabled',true);
+			},
+			success:function(res){
+				// $("cart-list").text(res.totalitems);
+				_vm.attr('disabled',false);
+				$("#cartList").html(res.data);
+			}
+		});
+		// End
+	});
+
+});
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Banner,Category,Brand,Product,ProductAttribute
-from django.db.models import Max,Min
+from django.db.models import Max,Min,Count
 from django.http import JsonResponse,HttpResponse
 from django.template.loader import render_to_string
 
@@ -64,7 +64,9 @@ def brand_product_list(request,brand_id):
 def product_detail(request,slug,id):
     product=Product.objects.get(id=id)
     related_products=Product.objects.filter(category=product.category).exclude(id=id)[:4] # show related products
-    return render(request,'product_detail.html',{'data':product,'related':related_products})
+    colors=ProductAttribute.objects.filter(product=product).values('color__id','color__title','color__color_code').distinct()#for product price
+    sizes=ProductAttribute.objects.filter(product=product).values('size__id','size__title','price','color__id').distinct()
+    return render(request,'product_detail.html',{'data':product,'related':related_products,'colors':colors,'sizes':sizes})
 
 
 
@@ -104,3 +106,75 @@ def load_more_data(request):
     data=Product.objects.all().order_by('-id')[offset:offset+limit]
     t=render_to_string('ajax/product-list.html',{'data':data})
     return JsonResponse({'data':t})
+
+
+# Add To Cart 
+def add_to_cart(request):
+    # del request.session['cartdata']
+    cart_p={}
+    cart_p[str(request.GET['id'])]={
+        'image':request.GET['image'],
+        'title':request.GET['title'],
+        'qty':request.GET['qty'],
+        'price':request.GET['price'],
+    }
+    if 'cartdata' in request.session:
+        if str(request.GET['id']) in request.session['cartdata']:
+            cart_data=request.session['cartdata']
+            cart_data[str(request.GET['id'])]['qty']=int(cart_p[str(request.GET['id'])]['qty'])
+            cart_data.update(cart_data)
+            request.session['cartdata']=cart_data
+        else:
+            cart_data=request.session['cartdata']
+            cart_data.update(cart_p)
+            request.session['cartdata']=cart_data
+    else:
+        request.session['cartdata']=cart_p
+
+    return JsonResponse({'data': request.session['cartdata'],'totalitems':len(request.session['cartdata'])})
+
+
+# Cart List Page
+def cart_list(request):
+    total_amt=0
+    for p_id,item in request.session['cartdata'].items():
+        total_amt+=int(item['qty'])*float(item['price'])
+    return render(request, 'cart.html',{'cart_data': request.session['cartdata'],'totalitems':len(request.session['cartdata']),'total_amt':total_amt})
+
+
+# Delete Cart Item
+def delete_cart_item(request):
+    p_id=str(request.GET['id'])
+    if 'cartdata' in request.session:
+        if p_id in request.session['cartdata']:
+            cart_data= request.session['cartdata']
+            del request.session['cartdata'][p_id]
+            request.session['cartdata']=cart_data
+    total_amt=0
+    for p_id,item in request.session['cartdata'].items():
+        total_amt+=int(item['qty'])*float(item['price'])
+    t=render_to_string('ajax/cart-list.html',{'cart_data': request.session['cartdata'],'totalitems':len(request.session[
+        'cartdata']),'total_amt':total_amt})
+    return JsonResponse({'data':t,'totalitems':len(request.session[
+        'cartdata'])})
+
+
+# Update Cart Item
+def update_cart_item(request):
+    p_id=str(request.GET['id'])
+    p_qty=request.GET['qty']
+    if 'cartdata' in request.session:
+        if p_id in request.session['cartdata']:
+            cart_data= request.session['cartdata']
+            cart_data[str(request.GET['id'])]['qty']=p_qty
+            request.session['cartdata']=cart_data
+    total_amt=0
+    for p_id,item in request.session['cartdata'].items():
+        total_amt+=int(item['qty'])*float(item['price'])
+    t=render_to_string('ajax/cart-list.html',{'cart_data': request.session['cartdata'],'totalitems':len(request.session[
+        'cartdata']),'total_amt':total_amt})
+    return JsonResponse({'data':t,'totalitems':len(request.session[
+        'cartdata'])})
+            
+
+
